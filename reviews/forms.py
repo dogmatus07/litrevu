ece4198cb1947186f4632522a7156d8d192ca275
Steve <steve.raharison@gmail.com>
2024-07-27 19:14:00 +0300

    Feature / Ticket management in progress

--- reviews/forms.py
+++ reviews/forms.py
@@ -3,9 +3,19 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import CustomUser, Ticket
 
 class CustomUserCreationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=False, help_text='Facultatif')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Facultatif')
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = ('email', 'first_name', 'last_name')
+        fields = ('email', 'first_name', 'last_name', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        if commit:
+            user.save()
+        return user
 
 class CustomUserAuthenticationForm(AuthenticationForm):
     class Meta:
